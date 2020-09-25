23a28bfc622858e5a82511ada1a7e688e53fa5b9
Nuelthesupreme <nuelthesupreme@gmail.com>
2020-09-25 22:26:02 +0100

    added heroku link to constants

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,6 +1,6 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "deployed api link";
+    : "https://mighty-wave-63765.herokuapp.com";
 
 export const REGISTER_URL = `${BASE_URL}/auth/register`;
