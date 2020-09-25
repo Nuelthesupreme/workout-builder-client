9da065d371ef5ba9ea9199c1fd5a2bef4bb10f35
Nuelthesupreme <nuelthesupreme@gmail.com>
2020-09-25 22:27:35 +0100

    Updates

---
+++ precache-manifest.53c6f41e84dde36a2748304f8915ab1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b22dc991b06eb20d38a609b998b6e58",
+    "url": "/workout-builder-client/index.html"
+  },
+  {
+    "revision": "1c57739de008c47bb657",
+    "url": "/workout-builder-client/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1c57739de008c47bb657",
+    "url": "/workout-builder-client/static/js/2.e2ac4366.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/workout-builder-client/static/js/2.e2ac4366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "834e3c341091e0533d48",
+    "url": "/workout-builder-client/static/js/main.2ed0361c.chunk.js"
+  },
+  {
+    "revision": "ccbee2dd31576e4d92cd",
+    "url": "/workout-builder-client/static/js/runtime-main.c7cdcfee.js"
+  }
+]);
